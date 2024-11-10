0acf4be571b756fe37915c6abe558cdff6b37a48
Denis <denis.kiselkov@list.ru>
2024-11-10 19:52:57 +0300

    documentation corrected

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
 # Grider
 
-![exemple how it work](https://github.com/KiselkovD/Grider/blob/main/exemple.png)
+<img src="https://github.com/KiselkovD/Grider/blob/main/res/exemple.png" alt="example how it work" height="200"/>
 
 ## About
 
-js program to cut images in matrix of images
+Grider is online js program to cut images in matrix of images
 
 ## How to use
 
+- open [Grider](https://kiselkovd.github.io/Grider/)
 - press **"выберете файл"** and choose image (`.png`; `.jpg`)
 - input number of rows & number of columns (black lines will display how picture will be cut)
 - press **"cut"**
--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 function createTable() {
 	var RowNum = document.getElementById('numberInput-h').value
-	var ColumNum = document.getElementById('numberInput-v').value
+	var ColumnNum = document.getElementById('numberInput-v').value
 
 	const canvas = document.getElementById('container')
 	var true_img = document.getElementById('true-img')
 	canvas.width = true_img.width
 	canvas.height = true_img.height
 	var ctx = canvas.getContext('2d')
+
+	//clear canvas and draw grid
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	ctx.beginPath()
 	var gridSize = canvas.width / RowNum
@@ -14,48 +16,49 @@ function createTable() {
 		ctx.moveTo(x, 0)
 		ctx.lineTo(x, canvas.height)
 	}
-	var gridSize = canvas.height / ColumNum
+	var gridSize = canvas.height / ColumnNum
 	for (var y = 0; y <= canvas.height; y += gridSize) {
 		ctx.moveTo(0, y)
 		ctx.lineTo(canvas.width, y)
 	}
-	ctx.strokeStyle = '#000000' // Цвет линий
+	ctx.strokeStyle = '#000000'
 	ctx.stroke()
 }
+
+// if number of columns or rows change, then canvas will be redrawn
 document.getElementById('numberInput-v').addEventListener('change', createTable)
 document.getElementById('numberInput-h').addEventListener('change', createTable)
-// Функция загрузки изображения
+
 function loadImage(event) {
-	const file = event.target.files[0] // Получаем выбранный файл
-	const reader = new FileReader() // Создаем новый объект FileReader
+	const file = event.target.files[0]
+	const reader = new FileReader()
 	reader.onload = function (e) {
-		const img = new Image() // Создаем новый элемент изображения
-		img.src = e.target.result // Устанавливаем источник изображения
+		const img = new Image()
+		img.src = e.target.result
+
+		// when file read and img load add img src to show it
 		img.onload = function () {
 			document.getElementById(
 				'img'
 			).innerHTML = `<img id="true-img" src="${img.src}">`
-			// Показываем кнопки для скачивания
 
 			document.getElementById('doWork').style.display = 'inline'
-
-			// Устанавливаем ссылки для скачивания половинок изображения
 		}
 	}
-	reader.readAsDataURL(file) // Читаем файл как Data URL
+	reader.readAsDataURL(file)
 }
 
-// Функция для установки ссылок на скачивание половинок изображения
-function setDownloadLinks() {
+function cutAndSetDownloadLinks() {
 	var RowNum = document.getElementById('numberInput-h').value
-	var ColumNum = document.getElementById('numberInput-v').value
+	var ColumnNum = document.getElementById('numberInput-v').value
 	const img = new Image()
 	img.src = document.getElementById('true-img').src
 	img.onload = function () {
 		Canvas_width = img.width / RowNum
-		Canvas_height = img.height / ColumNum
-		/*alert(`RowNum ${RowNum} ColumNum ${ColumNum}`)*/
-		for (var y = 0; y < ColumNum; y += 1) {
+		Canvas_height = img.height / ColumnNum
+
+		// cutting into parts with drawImage in NewCanvas
+		for (var y = 0; y < ColumnNum; y += 1) {
 			for (var x = 0; x < RowNum; x += 1) {
 				const NewCanvas = document.createElement('canvas')
 				document.getElementById('output-part').appendChild(NewCanvas)
@@ -73,19 +76,24 @@ function setDownloadLinks() {
 					Canvas_width,
 					Canvas_height
 				)
+
+				// add URLs
 				NewCanvas.onclick = function () {
 					downloadImage(NewCanvas.toDataURL(), 'part.png')
 				}
 			}
-			document.getElementById('output-part').appendChild(document.createElement('div'))
+
+			// add empty div to line break
+			document
+				.getElementById('output-part')
+				.appendChild(document.createElement('div'))
 		}
 	}
 }
 
-// Функция для скачивания изображения по Data URL
 function downloadImage(dataUrl, filename) {
-	const link = document.createElement('a') // Создаем элемент ссылки
-	link.href = dataUrl // Устанавливаем адрес ссылки на Data URL
-	link.download = filename // Устанавливаем имя файла для скачивания
-	link.click() // Имитируем клик по ссылке для начала скачивания
+	const link = document.createElement('a')
+	link.href = dataUrl
+	link.download = filename
+	link.click()
 }
